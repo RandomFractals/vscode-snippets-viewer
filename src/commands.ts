79747496356f148e9ecdb297c16301f5f2f29491
Taras Novak <taras.novak@randomfractals.com>
2021-03-23 12:51:37 -0500

    add open snippet file context menu item and command for snippet files and snippets in tree view (#4)

--- src/commands.ts
+++ src/commands.ts
@@ -1,8 +1,20 @@
-import { 
+import {
+  DocumentSymbol,
   ExtensionContext,
-  commands
+  Selection,
+  TextEditorRevealType,
+  TextDocument,
+  Uri,
+  commands,
+  window,
+  workspace
 } 
 from 'vscode';
+import {
+	SnippetFile, 
+	Snippet
+} 
+from './snippets/snippets'
 
 export function registerCommands(context: ExtensionContext) {
   context.subscriptions.push(
@@ -19,10 +31,51 @@ export function registerCommands(context: ExtensionContext) {
   );
 
   context.subscriptions.push(
+    commands.registerCommand(`snippets.viewer.openSnippetFile`, (snippet: SnippetFile | Snippet) => {
+      let filePath: string;
+      if (snippet instanceof Snippet) {
+        filePath = snippet.snippetFile.filePath;
+      }
+      else {
+        filePath = snippet.filePath;
+      }
+      workspace.openTextDocument(Uri.file(filePath)).then((document) => {
+        window.showTextDocument(document).then(() => {
+          goToSymbol(document, snippet.label);
+        });
+      });
+    })
+  );
+
+  context.subscriptions.push(
     commands.registerCommand(`snippets.viewer.viewSettings`, viewSettings)
   );
 }
 
 async function viewSettings() {
   commands.executeCommand( 'workbench.action.openSettings', 'snippets.viewer' );
+}
+
+async function goToSymbol(document: TextDocument, symbolName: string) {
+  const symbols = await getSymbols(document);
+  const findSymbol = symbols.find(symbol => symbol.name === symbolName);
+  const activeTextEditor = window.activeTextEditor;
+  if (findSymbol && activeTextEditor) {
+    activeTextEditor.selection = new Selection(findSymbol.range.start, findSymbol.range.start);
+    activeTextEditor.revealRange(findSymbol.range, TextEditorRevealType.AtTop);
+  }
+}
+
+async function getSymbols(document: TextDocument): Promise<DocumentSymbol[]> {
+  return new Promise(async (resolve, reject) => {
+    let symbols = await commands.executeCommand<DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', document.uri);// tslint:disable-line
+    if (!symbols || symbols.length === 0) {
+      setTimeout(async () => {
+        symbols = await commands.executeCommand<DocumentSymbol[]>('vscode.executeDocumentSymbolProvider', document.uri);// tslint:disable-line
+        return resolve(symbols || []);
+      }, 1200);
+    } else {
+      return resolve(symbols || []);
+    }
+  });
 }
